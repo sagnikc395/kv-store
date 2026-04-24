bd119771aa24b724baaf7dc0aef53e51524526b5
Sagnik Chatterjee <sagnikchatterjee607@gmail.com>
2026-04-24 00:09:54 -0400

    feat: added the start_election method

--- kvstore/raft/raft.py
+++ kvstore/raft/raft.py
@@ -112,3 +112,55 @@ class ConsensusModule:
                 if time.monotonic() - self.election_reset_event >= timeout:
                     self._start_election()
                     return
+
+    def _start_election(self):
+        """
+        Transition to Candidate and send RequestVote RPCs.
+        Expects mu hold
+        """
+        self.state = CMState.Candidate
+        self.current_term += 1
+        saved_term = self.current_term
+        self.election_reset_event = time.monotonic()
+        self.voted_for = self.id
+        self.dlog("becomes Candidate (current_term=%d); log=%s", saved_term, self.log)
+
+        votes_received = 1
+
+        for peer_id in self.peer_ids:
+
+            def _send_vote(peer_id: int = peer_id) -> None:
+                args = RequestVoteArgs(
+                    term=saved_term,
+                    candidate_id=self.id,
+                )
+                self.dlog("sending RequestVote to %d: %s", peer_id, args)
+                try:
+                    reply_data = self.server.call(peer_id, "RequestVote", args.__dict__)
+                    reply = RequestVoteReply(**reply_data)
+                except Exception:
+                    return
+
+                nonlocal votes_received
+                with self.mu:
+                    self.dlog("received RequestVoteReply %s", reply)
+
+                    if self.state != CMState.Candidate:
+                        self.dlog("while waiting for reply, state=%s", self.state)
+                        return
+
+                    if reply.term > saved_term:
+                        self.dlog("term out of date in RequestVoteReply")
+                        self._become_follower(reply.term)
+                        return
+
+                    if reply.term == saved_term and reply.vote_granted:
+                        votes_received += 1
+                        if votes_received * 2 > len(self.peer_ids) + 1:
+                            self.dlog("wins election with %d votes", votes_received)
+                            self._start_leader()
+
+            threading.Thread(target=_send_vote, daemon=True).start()
+
+        # Fallback timer in case this election fails.
+        threading.Thread(target=self.run_election_timer, daemon=True).start()
